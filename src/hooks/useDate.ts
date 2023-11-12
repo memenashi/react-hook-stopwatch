c5c01423fb51849b5e62f7628098839a7355ddd5
memenashi <shunsuke.vrml.frm@gmail.com>
2023-11-13 00:54:57 +0900

    using useLocalStorage

--- src/hooks/useDate.ts
+++ src/hooks/useDate.ts
@@ -1,19 +1,14 @@
 import { useState } from "react";
+import useLocalStorage from "react-use/lib/useLocalStorage";
 
 import { UseDateOption, UseDateReturn } from "../types/useDateOption";
 import { NullableDate } from "../types/nullableDate";
 
-import { useStorageState } from "./useStorageState";
-
 export function useDate({ defaultValue, key, mode }: UseDateOption): UseDateReturn {
   const [time, setTime] = useState<NullableDate>(defaultValue);
-  const [localTime, setLocalTime] = useStorageState<NullableDate>(
+  const [localTime, setLocalTime] = useLocalStorage<NullableDate>(
     `time_${key ?? ""}`,
-    defaultValue,
-    (_key, value) => {
-      if (value === null) return value;
-      if (typeof value === "string" || typeof value === "number") return new Date(value);
-    }
+    defaultValue
   );
   const isState = mode == "state";
   return { date: isState ? time : localTime, setDate: isState ? setTime : setLocalTime };
